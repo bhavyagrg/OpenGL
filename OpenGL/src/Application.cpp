c3a9119d403c1cc129384327532d36b7f48ee7ab
Bhavya Garg <bebhavyagarg@gmail.com>
2023-05-09 16:28:20 +0530

    Made a traingle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -10,7 +10,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Hello", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -26,6 +26,12 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
@@ -34,5 +40,6 @@ int main(void)
     }
 
     glfwTerminate();
+    
     return 0;
 }
